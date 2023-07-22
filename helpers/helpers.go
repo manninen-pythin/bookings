ab3c515fff31453d7d44ae7ac8a87ed4ca11d3e2
Eric <67717846+manninen-pythin@users.noreply.github.com>
2023-07-22 15:20:52 -0400

    Working with converting JSON data

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -9,6 +9,13 @@ type SomeType struct {
 	TypeName string
 }
 
+type Person struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	HairColor string `json:"hair_color"`
+	HasDog    bool   `json:"has_dog"`
+}
+
 func RandomNumber(n int) int {
 	rand.Seed(time.Now().UnixNano())
 	value := rand.Intn(n)
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 
 	"github.com/manninen-pythin/bookings/helpers"
@@ -21,4 +23,55 @@ func main() {
 
 	num := <-intChan
 	log.Println(num)
+
+	myJson := `
+	[
+		{
+			"first_name": "Clark",
+			"last_name": "Kent",
+			"hair_color": "black",
+			"has_dog": true
+		},
+
+		{
+			"first_name": "Bruce",
+			"last_name": "Wayne",
+			"hair_color": "black",
+			"has_dog": false
+		}
+	]`
+
+	var unmarshalled []helpers.Person
+
+	err := json.Unmarshal([]byte(myJson), &unmarshalled)
+	if err != nil {
+		log.Println("Error unmarshalling json", err)
+	}
+	log.Printf("unmarshalled: %v", unmarshalled)
+
+	// write json from a struct
+	var mySlice []helpers.Person
+
+	var m1 helpers.Person
+	m1.FirstName = "Wally"
+	m1.LastName = "West"
+	m1.HairColor = "red"
+	m1.HasDog = false
+
+	var m2 helpers.Person
+	m2.FirstName = "Diana"
+	m2.LastName = "Prince"
+	m2.HairColor = "black"
+	m2.HasDog = false
+
+	mySlice = append(mySlice, m1, m2)
+
+	newJson, err := json.MarshalIndent(mySlice, "", "     ")
+
+	if err != nil {
+		log.Println("error:", err)
+	}
+
+	fmt.Println(string(newJson))
+
 }
